b25b60e5453239ef01befd34b0424120167e023e
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-11-16 18:21:52 +0200

    done with navbar responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,42 @@
 import React, { Fragment } from "react";
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavItem,
+  NavLinks,
+  NavMenu,
+  NavLogo,
+  NavbarContainer,
+  MobileIcon,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
 const Navbar = () => {
   return (
     <Fragment>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">SMOOTH</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </Fragment>
