138b3f7fd1f795689dc95a0bd484c03dde1df0d8
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-11-17 17:11:00 +0200

    add info Section with styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React, { Fragment, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
+import { homeObjOne } from "../components/InfoSection/Data";
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => {
@@ -12,7 +14,8 @@ const Home = () => {
     <Fragment>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
-      <HeroSection/>
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
     </Fragment>
   );
 };
